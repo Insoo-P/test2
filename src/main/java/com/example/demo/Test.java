c7db81ae8a1c13ba2b420a39f5bc2b918851b4d9
박인수 <insoodever@gmail.com>
2024-09-24 00:25:54 +0900

    2-1 commit

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -2,11 +2,10 @@ package com.example.demo;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("안녕하세요10");
-        System.out.println("안녕하세요20");
-        System.out.println("안녕하세요30");
-        System.out.println("안녕하세요40");
-        System.out.println("안녕하세요60");
-        System.out.println("안녕하세요70");
+        System.out.println("안녕하세요1");
+        System.out.println("안녕하세요2");
+        System.out.println("안녕하세요3");
+        System.out.println("안녕하세요4");
+        System.out.println("안녕하세요5");
     }
 }
